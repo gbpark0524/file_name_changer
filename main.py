a35d11c3551d64086730430613be7f6e0e2da388
gbpark0524 <gbpark0524@gmail.com>
2022-03-07 00:57:58 +0900

    fix names by naming rule

--- main.py
+++ main.py
@@ -7,63 +7,51 @@ try:
         setting = json.load(settingFile)
 except:
     setting = dict()
-    setting['inPath'] = 'in'
-    setting['outPath'] = 'out'
+    setting['path_in'] = 'in'
+    setting['path_out'] = 'out'
     setting['extension'] = 'txt'
 
 
-inPath = setting['inPath']
-outPath = setting['outPath']
+path_in = setting['path_in']
+path_out = setting['path_out']
 ext = setting['extension']
 
 root = Tk()
 # root.geometry("600x400")
 root.title("fileNameChanger")
 root.resizable(False, False)
-titleLabel = Label(root, text='[file name changer]')
-# titleLabel = Label(root, text='[file name changer]', borderwidth=2, relief="groove")
+title_label = Label(root, text='[file name changer]')
+# title_label = Label(root, text='[file name changer]', borderwidth=2, relief="groove")
 
-inPathButton = Button(root, text='input file path')
-inPathEntry = Entry(root, width=30)
-inPathEntry.insert(0, inPath)
+btn_path_in = Button(root, text='input file path')
+entry_path_in = Entry(root, width=30)
+entry_path_in.insert(0, path_in)
 
-outPathButton = Button(root, text='output file path')
-outPathEntry = Entry(root, width=30)
-outPathEntry.insert(0, outPath)
+btn_path_out = Button(root, text='output file path')
+entry_path_out = Entry(root, width=30)
+entry_path_out.insert(0, path_out)
 
-dataPathButton = Button(root, text='json data file')
-dataPathEntry = Entry(root, width=30)
-dataPathEntry.insert(0, outPath)
+btn_path_data = Button(root, text='json data file')
+entry_path_data = Entry(root, width=30)
+entry_path_data.insert(0, path_out)
 
-extPathLabel = Label(root, text='[ext]')
-extPathEntry = Entry(root, width=4)
-extPathEntry.insert(0, ext)
+label_ext = Label(root, text='[ext]')
+entry_ext = Entry(root, width=4)
+entry_ext.insert(0, ext)
 
-saveButton = Button(root, text='setting save')
-changeButton = Button(root, text='file name change')
+btn_save = Button(root, text='setting save')
+btn_change = Button(root, text='file name change')
 
-titleLabel.grid(row=0, column=0, padx=5, pady=5, columnspan=3, sticky="ew")
-inPathButton.grid(row=1, column=0, padx=5, pady=2, sticky="ew")
-inPathEntry.grid(row=1, column=1, padx=5, pady=2)
-outPathButton.grid(row=2, column=0, padx=5, pady=2, sticky="ew")
-outPathEntry.grid(row=2, column=1, padx=5, pady=2)
-dataPathButton.grid(row=3, column=0, padx=5, pady=2, sticky="ew")
-dataPathEntry.grid(row=3, column=1, padx=5, pady=2)
-extPathLabel.grid(row=2, column=2, padx=5, pady=2, sticky="ew")
-extPathEntry.grid(row=3, column=2, padx=5, pady=2)
-saveButton.grid(row=4, column=0, padx=5, pady=2, sticky="ew")
-changeButton.grid(row=4, column=1, columnspan=2, padx=5, pady=2, sticky="ew")
+title_label.grid(row=0, column=0, padx=5, pady=5, columnspan=3, sticky="ew")
+btn_path_in.grid(row=1, column=0, padx=5, pady=2, sticky="ew")
+entry_path_in.grid(row=1, column=1, padx=5, pady=2)
+btn_path_out.grid(row=2, column=0, padx=5, pady=2, sticky="ew")
+entry_path_out.grid(row=2, column=1, padx=5, pady=2)
+btn_path_data.grid(row=3, column=0, padx=5, pady=2, sticky="ew")
+entry_path_data.grid(row=3, column=1, padx=5, pady=2)
+label_ext.grid(row=2, column=2, padx=5, pady=2, sticky="ew")
+entry_ext.grid(row=3, column=2, padx=5, pady=2)
+btn_save.grid(row=4, column=0, padx=5, pady=2, sticky="ew")
+btn_change.grid(row=4, column=1, columnspan=2, padx=5, pady=2, sticky="ew")
 
 root.mainloop()
-# data = {"inPath":"C:\Temp\in","outPath":"C:\Temp\out"}
-
-# with open('setting.json', 'w') as fp:
-#     json.dump(data, fp)
-
-
-# for name in inNames:
-#     fName = name[:-4]
-#     if data.get(fName) != None :
-#         chName = data.get(fName) + '.hwp'
-#         print(fName + ' => change => ' + chName)
-#         os.rename(inPath + '\\' + name , outPath + '\\' + chName)
