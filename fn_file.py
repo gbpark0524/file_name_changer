2824823c129492e7542f71973cf26aad41e33f93
gbpark0524 <gbpark0524@gmail.com>
2022-03-10 11:05:15 +0900

    modified events and refactoring

--- fn_file.py
+++ fn_file.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 
 def get_setting():
@@ -6,9 +7,13 @@ def get_setting():
         with open('setting.json', 'r') as settingFile:
             setting = json.load(settingFile)
     except IOError:
+        dir_in = os.path.join(os.curdir, 'in')
+        dir_out = os.path.join(os.curdir, 'out')
+
         setting = dict()
-        setting['path_in'] = 'in'
-        setting['path_out'] = 'out'
+        setting['path_in'] = dir_in
+        setting['path_out'] = dir_out
+        setting['json'] = os.curdir
         setting['extension'] = 'txt'
 
     return setting
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from tkinter import *
 from tkinter import filedialog
 from fn_file import *
 
-
 root = Tk()
 # root.geometry("600x400")
 root.title("fileNameChanger")
@@ -12,50 +11,53 @@ root.resizable(False, False)
 title_label = Label(root, text='[file name changer]')
 # title_label = Label(root, text='[file name changer]', borderwidth=2, relief="groove")
 
-btn_path_in = Button(root, text='input file path')
-entry_path_in = Entry(root, width=30)
-btn_path_out = Button(root, text='output file path')
-entry_path_out = Entry(root, width=30)
-btn_path_data = Button(root, text='json data file')
 entry_path_data = Entry(root, width=30)
+entry_path_in = Entry(root, width=30)
+entry_path_out = Entry(root, width=30)
 label_ext = Label(root, text='[ext]')
 entry_ext = Entry(root, width=4)
-btn_save = Button(root, text='setting save')
-btn_change = Button(root, text='file name change')
 
-title_label.grid(row=0, column=0, padx=5, pady=5, columnspan=3, sticky="ew")
-btn_path_in.grid(row=1, column=0, padx=5, pady=2, sticky="ew")
-entry_path_in.grid(row=1, column=1, padx=5, pady=2)
-btn_path_out.grid(row=2, column=0, padx=5, pady=2, sticky="ew")
-entry_path_out.grid(row=2, column=1, padx=5, pady=2)
-btn_path_data.grid(row=3, column=0, padx=5, pady=2, sticky="ew")
-entry_path_data.grid(row=3, column=1, padx=5, pady=2)
-label_ext.grid(row=2, column=2, padx=5, pady=2, sticky="ew")
+
+# TODO - call initial dir from entry
+def get_path_in():
+    entry_path_in.delete(0, END)
+    entry_path_in.insert(0, filedialog.askdirectory(initialdir=os.curdir, title="select folder"))
+
+
+def get_path_out():
+    entry_path_out.delete(0, END)
+    entry_path_out.insert(0, filedialog.askdirectory(initialdir=os.curdir, title="select folder"))
+
+
+def get_path_data():
+    entry_path_data.delete(0, END)
+    entry_path_data.insert(0, filedialog.askopenfilename(initialdir=os.curdir,
+                                                         title="select json file", filetypes=[("JSON files", ".json")]))
+
+
+btn_help_data = Button(root, text='[?]', overrelief=SOLID)
+btn_path_data = Button(root, text='json data file', overrelief=SOLID, command=get_path_data)
+btn_path_in = Button(root, text='input file path', overrelief=SOLID, command=get_path_in)
+btn_path_out = Button(root, text='output file path', relief=RAISED, command=get_path_out)
+btn_save = Button(root, text='setting save', overrelief=SOLID)
+btn_change = Button(root, text='file name change', overrelief=SOLID)
+
+title_label.grid(row=0, column=0, padx=5, pady=5, columnspan=3, sticky=EW)
+btn_path_data.grid(row=1, column=0, padx=5, pady=2, sticky=EW)
+entry_path_data.grid(row=1, column=1, padx=5, pady=2)
+btn_path_in.grid(row=2, column=0, padx=5, pady=2, sticky=EW)
+entry_path_in.grid(row=2, column=1, padx=5, pady=2)
+btn_path_out.grid(row=3, column=0, padx=5, pady=2, sticky=EW)
+entry_path_out.grid(row=3, column=1, padx=5, pady=2)
+btn_help_data.grid(row=2, column=2, padx=5, pady=2, sticky=EW)
+label_ext.grid(row=2, column=2, padx=5, pady=2, sticky=EW)
 entry_ext.grid(row=3, column=2, padx=5, pady=2)
-btn_save.grid(row=4, column=0, padx=5, pady=2, sticky="ew")
-btn_change.grid(row=4, column=1, columnspan=2, padx=5, pady=2, sticky="ew")
+btn_save.grid(row=4, column=0, padx=5, pady=2, sticky=EW)
+btn_change.grid(row=4, column=1, columnspan=2, padx=5, pady=2, sticky=EW)
 
 setting = get_setting()
 entry_path_in.insert(0, setting['path_in'])
 entry_path_out.insert(0, setting['path_out'])
 entry_ext.insert(0, setting['extension'])
-
-def get_path_in(event):
-    entry_path_in.delete(0, END)
-    entry_path_in.insert(0, filedialog.askdirectory(initialdir="/", title="select folder"))
-
-def get_path_out(event):
-    entry_path_out.delete(0, END)
-    entry_path_out.insert(0, filedialog.askdirectory(initialdir="/", title="select folder"))
-
-def get_path_data(event):
-    entry_path_data.delete(0, END)
-    entry_path_data.insert(0, filedialog.askopenfilename(initialdir="/",
-                                                         title="select json file", filetypes=[("JSON files", ".json")]))
-
-
-btn_path_in.bind('<Button-1>', get_path_in)
-btn_path_out.bind('<Button-1>', get_path_out)
-btn_path_data.bind('<Button-1>', get_path_data)
 
 root.mainloop()
